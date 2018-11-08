fde8d6e096551a557c9f602b4fd06381c81e9a7f
Ruoxuan95 <rx65@cornell.edu>
2018-11-07 21:57:51 -0500

    no bug

--- src/samples/input/queries.sql
+++ src/samples/input/queries.sql
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 SELECT * FROM Sailors, Boats WHERE Sailors.A > 9900 AND Boats.E > 5000
-=======
-SELECT * FROM Sailors, Boats WHERE Sailors.A > 9900 AND Boats.E > 5000
->>>>>>> 1a9d2f2834d71a8cf289eb9f9df39b0545c100aa
